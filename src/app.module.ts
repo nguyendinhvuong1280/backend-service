a8aa4dbaff184970d35fa221d609250b3b3b5cc8
nguyendinhvuong1280 <vuongnd@edsolabs.com>
2023-03-30 23:57:31 +0700

    add log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WinstonModule } from 'nest-winston';
+import { WINSTON_CONFIG } from '@synodus/nestjs-kafkajs-wrapper';
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot(WINSTON_CONFIG),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from '@synodus/nestjs-kafkajs-wrapper';
 
+const logger: Logger = new Logger('Main');
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(+process.env.PORT || 3000, '0.0.0.0');
+  console.log('+process.env.PORT: ', +process.env.PORT)
+  app.useLogger(process.env.ENABLE_LOG === 'true' ? app.get(WINSTON_MODULE_NEST_PROVIDER) : false);
+
+  logger.log(`Worker ${process.pid} started on address ${await app.getUrl()}`);
 }
 bootstrap();
